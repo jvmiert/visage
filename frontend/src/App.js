021206bd73d60a0f469b4b03a88d5b33b86a09f2
Jeroen <jvmiert@gmail.com>
2021-03-14 16:01:24 +0700

    update frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,41 @@
 import React, { useState, useEffect, useRef } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
   const videoEl = useRef(null);
 
+  // useEffect(() => {
+  //   async function loadVideo() {
+  //     navigator.mediaDevices
+  //       .getUserMedia({
+  //         audio: false,
+  //         video: true,
+  //       })
+  //       .then(
+  //         (stream) => (videoEl.current.srcObject = stream),
+  //         (err) => console.log(err)
+  //       );
+  //   }
+  //   loadVideo();
+  // }, []);
+
   useEffect(() => {
-    async function loadVideo() {
-      navigator.mediaDevices
-        .getUserMedia({
-          audio: false,
-          video: true,
-        })
-        .then(
-          (stream) => (videoEl.current.srcObject = stream),
-          (err) => console.log(err)
-        );
-    }
-    loadVideo();
+    const fetchData = async () => {
+      const result = await axios(
+        '/api/test',
+      );
+ 
+      console.log(result.data);
+    };
+ 
+    fetchData();
   }, []);
+
   return (
     <div>
-      <video ref={videoEl} autoPlay playsInline></video>
+      {/*<video ref={videoEl} autoPlay playsInline></video>*/}
+      <p>Hello!</p>
     </div>
   );
 }
