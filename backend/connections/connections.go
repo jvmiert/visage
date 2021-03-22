e3114c0530fff2eada2652e34b2fbc83a54d0387
Jeroen <jvmiert@gmail.com>
2021-03-22 23:30:12 +0700

    change tabs into spaces for redis connect backend

--- backend/connections/connections.go
+++ backend/connections/connections.go
@@ -1,15 +1,13 @@
 package connections
 
-
 import (
   "github.com/go-redis/redis/v8"
 )
 
-
 func RClient() *redis.Client {
-	client := redis.NewClient(&redis.Options{
-	Addr: "localhost:6379",
-	})
+  client := redis.NewClient(&redis.Options{
+    Addr: "localhost:6379",
+  })
 
-	return client 
+  return client
 }
