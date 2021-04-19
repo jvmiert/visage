09e0f1e8e5ef658cc01f8fae02a5b5aed4b761d2
Jeroen <jvmiert@gmail.com>
2021-04-19 21:42:49 +0700

    add debug messages

--- frontend/lib/ionClient.js
+++ frontend/lib/ionClient.js
@@ -116,7 +116,7 @@ const loadClient = async function load(
       //console.log("(publisher) adding track: ", event);
     };
     pcSub.ontrack = function (event) {
-      //console.log("(subscriber) adding track: ", event);
+      console.log("(subscriber) adding track: ", event);
       if (event.track.kind === "audio") {
         return;
       }
@@ -143,6 +143,7 @@ const loadClient = async function load(
       setState((prevState) => {
         const newStreamList = prevState.streams.concat(event.streams[0]);
         const showValue = newStreamList.length > 0;
+        console.log("New stream list: ", newStreamList);
         return {
           ...prevState,
           ...{
