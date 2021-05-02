3e0bb44147bf6d4fb275d0ebeee3736a4f1f84e0
Jeroen <jvmiert@gmail.com>
2021-05-02 12:30:01 +0700

    add stepper

--- frontend/pages/[roomID]/setup/[[...phase]].js
+++ frontend/pages/[roomID]/setup/[[...phase]].js
@@ -1,6 +1,10 @@
 import { useRouter } from "next/router";
 import { t, Trans } from "@lingui/macro";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDotCircle } from "@fortawesome/free-solid-svg-icons";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+
 import {
   PermissionSetup,
   VideoSetup,
@@ -38,6 +42,22 @@ export default function RoomSetupView() {
   return (
     <div className="min-h-screen w-full bg-gradient-to-t from-purple-50 to-white p-5">
       <div className="mx-auto max-w-lg px-6 py-8 bg-white border-0 shadow-md rounded-lg">
+        <div className="w-full mb-4 flex justify-between">
+          {Object.entries(SetupState).map(([key, value]) => {
+            const passed = SetupState[phase].order > value.order;
+            return (
+              <div key={key} className="flex flex-col items-center">
+                <FontAwesomeIcon
+                  className={`${
+                    passed ? "text-indigo-400" : "text-gray-600"
+                  } mb-2`}
+                  icon={passed ? faCheckCircle : faDotCircle}
+                />
+                <p className="text-xs">{value.name}</p>
+              </div>
+            );
+          })}
+        </div>
         {renderStep(phase)}
       </div>
     </div>
