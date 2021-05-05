c1e7118467344b2cb127b0ba46f6259ad5e8711c
Jeroen <jvmiert@gmail.com>
2021-05-06 00:07:05 +0700

    implement 1 - 4 person mobile app room view

--- mobile/VisageMobile/src/Room.js
+++ mobile/VisageMobile/src/Room.js
@@ -11,6 +11,8 @@ import {
 
 import { RTCView, registerGlobals } from 'react-native-webrtc';
 
+import { useHeaderHeight } from '@react-navigation/stack';
+
 import useStore from './lib/store';
 
 registerGlobals();
@@ -34,18 +36,40 @@ const styles = StyleSheet.create({
   video: {
     flex: 1,
   },
+  videoLarge: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   borderContainer: {
     overflow: 'hidden',
-    borderRadius: 20,
-    width: '100%',
-    height: '100%',
+    borderRadius: 10,
     backgroundColor: 'black',
+    flex: 1,
   },
   itemContainer: {
-    width: windowWidth,
-    //height: windowWidth / 2,
-    padding: 8,
-    aspectRatio: 1.7778,
+    padding: 5,
+  },
+  videoSelfContainer: {
+    height: 100,
+    width: 70,
+    marginRight: 10,
+    marginTop: -10,
+    borderRadius: 5,
+    overflow: 'hidden',
+    backgroundColor: 'black',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+  selfContainer: {
+    position: 'absolute',
+    right: 0,
   },
 });
 
@@ -55,13 +79,15 @@ const webrtcConfig = {
   iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
 };
 
-export default function Room({ route }) {
+export default function Room({ route, navigation }) {
+  const headerHeight = useHeaderHeight();
   const { room, wsToken } = route.params;
 
   const addStream = useStore(useCallback(state => state.addStream, []));
   const removeStream = useStore(useCallback(state => state.removeStream, []));
   const streams = useStore(useCallback(state => state.streams, []));
   const client = useStore(useCallback(state => state.client, []));
+  const selfStream = useStore(useCallback(state => state.selfStream, []));
   const set = useStore(useCallback(state => state.set, []));
 
   const loadIon = useCallback(
@@ -86,6 +112,10 @@ export default function Room({ route }) {
 
         ionClient.publish(local);
 
+        set(state => {
+          state.selfStream = local;
+        });
+
         ionClient.transports[1].pc.onaddstream = (e: any) => {
           //console.log('on add stream: ', e);
           addStream(e.stream);
@@ -101,8 +131,11 @@ export default function Room({ route }) {
   );
 
   useEffect(() => {
-    loadIon(room, wsToken);
-  }, [room, wsToken, loadIon]);
+    if (!selfStream) {
+      loadIon(room, wsToken);
+      navigation.setOptions({ headerTitle: room });
+    }
+  }, [room, wsToken, loadIon, navigation, selfStream]);
 
   useEffect(() => {
     const cleanClient = client;
@@ -116,26 +149,78 @@ export default function Room({ route }) {
         set(state => {
           state.streams = [];
         });
+        set(state => {
+          state.selfStream = null;
+        });
       }
     };
   }, [client, set]);
 
+  const getWidth = useCallback(() => {
+    const participants = streams.length;
+
+    if (participants < 3) {
+      return '100%';
+    }
+    if (participants > 2) {
+      return '50%';
+    }
+  }, [streams]);
+
   return (
     <SafeAreaView>
-      <StatusBar />
-      <Text>{streams.length}</Text>
-      <View style={styles.container}>
-        {streams.map(s => (
-          <View key={s.toURL()} style={styles.itemContainer}>
-            <View style={styles.borderContainer}>
-              <RTCView
-                objectFit="contain"
-                streamURL={s.toURL()}
-                style={styles.video}
-              />
-            </View>
+      <StatusBar barStyle={'dark-content'} />
+      {selfStream && (
+        <View
+          style={[
+            styles.selfContainer,
+            {
+              top: windowHeight - headerHeight - StatusBar.currentHeight - 100,
+            },
+          ]}>
+          <View style={styles.videoSelfContainer}>
+            <RTCView
+              objectFit="cover"
+              streamURL={selfStream.toURL()}
+              style={styles.video}
+              zOrder={1}
+            />
           </View>
-        ))}
+        </View>
+      )}
+      <View style={styles.container}>
+        {streams.length > 0 && streams.length < 2 && (
+          <RTCView
+            objectFit="cover"
+            streamURL={streams[0].toURL()}
+            style={[
+              styles.videoLarge,
+              { height: windowHeight - headerHeight, width: windowWidth },
+            ]}
+          />
+        )}
+        {streams.length > 1 &&
+          streams.map(s => (
+            <View
+              key={s.toURL()}
+              style={[
+                styles.itemContainer,
+                {
+                  height:
+                    (windowHeight - headerHeight - StatusBar.currentHeight) / 2,
+                  width: getWidth(),
+                },
+              ]}>
+              <View style={styles.borderContainer}>
+                <RTCView
+                  objectFit="cover"
+                  streamURL={s.toURL()}
+                  style={styles.video}
+                  zOrder={0}
+                />
+              </View>
+            </View>
+          ))}
       </View>
     </SafeAreaView>
   );
